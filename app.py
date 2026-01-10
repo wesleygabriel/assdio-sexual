fbb86c72a67f919da3a7f72b6f9a69decd2ffe6c
capir.2024116isinf0003 <capir.2024116isinf0003@aluno.ifpi.edu.br>
2026-01-10 18:18:20 +0000

    fix: correção das rotas

--- app.py
+++ app.py
@@ -1,15 +1,28 @@
 import os
 
-from flask import Flask, send_file
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return send_file('src/index.html')
+    return render_template('index.html')
+
+@app.route("/contato")
+def contact():
+    return render_template('contact.html')
+
+@app.route("/login")
+def login():
+    return render_template('login.html')
+
+@app.route("/desabafo")
+def desabafo():
+    return render_template('desabafo.html')
+
 
 def main():
-    app.run(port=int(os.environ.get('PORT', 80)))
+    app.run(port=int(os.environ.get('PORT', 5000)))
 
 if __name__ == "__main__":
     main()
